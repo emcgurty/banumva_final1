8cf5355beb466bf85b6f62c80cecb82e7aa1dc3f
emcgurty <emcgurty3@comcast.net>
2014-01-09 10:32:37 -0500

    Update application.rb

--- banumva.org/config/application.rb
+++ banumva.org/config/application.rb
@@ -13,7 +13,7 @@ module Petitions
         require 'collaborator_values'
     include CollaboratorValues
     CollaboratorMethods.load_values
-config.secret_token = 'c7ddjhdu5b6374e8a08639411cf95f9722b7e115849ae902fbe0c8a80bc26d0871d1c5a24439555sdfsddf5d21369'
+config.secret_token = ''
     
   end
 end
